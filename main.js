3c53d72fecfc3c6d1f9ddf2cb66c2cef6d153443
Paulo Viana <pauloviana@MacBook-Pro-de-Paulo.local>
2019-04-28 21:12:09 -0300

    Criação da primeira janela.

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const { app, BrowserWindow } = require('electron');
+
+
+app.on('ready', () => {
+    console.log('App started');
+
+    let mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+    
+});
